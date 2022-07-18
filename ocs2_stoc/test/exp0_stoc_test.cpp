a9aff43ff7a7442aa54363ecc9c5cdafb6b0ae5c
Sotaro Katayama <ihtf4ta7f@gmail.com>
2022-07-18 17:03:25 +0900

    add ipm oc constructor from ocs2 oc

--- ocs2_stoc/test/exp0_stoc_test.cpp
+++ ocs2_stoc/test/exp0_stoc_test.cpp
@@ -2,17 +2,20 @@
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
+#include <memory>
 
+#include <ocs2_core/initialization/DefaultInitializer.h>
 #include <ocs2_oc/test/EXP0.h>
 
 #include <ocs2_stoc/STOC.h>
 
 using namespace ocs2;
 
-enum { STATE_DIM = 2, INPUT_DIM = 1 };
-
 TEST(exp0_stoc_test, exp0_stoc_test) {
-  STOC_Settings settings;
+  static constexpr size_t STATE_DIM = 2;
+  static constexpr size_t INPUT_DIM = 1;
+
+  stoc::Settings settings;
   settings.numIteration  = 10;
   settings.primalFeasTol = 1e-6;
   settings.dualFeasTol   = 1e-4;
@@ -27,89 +30,60 @@ TEST(exp0_stoc_test, exp0_stoc_test) {
   settings.printLinesearch = false;
   settings.nThreads = 4;
 
-  // logic rule
   std::vector<double> initEventTimes{1.0};
-  std::vector<size_t> subsystemsSequence{0, 1};
-  std::shared_ptr<ModeScheduleManager<STATE_DIM, INPUT_DIM>> modeScheduleManagerPtr(
-      new ModeScheduleManager<STATE_DIM, INPUT_DIM>({initEventTimes, subsystemsSequence}));
+  std::vector<size_t> modeSequence{0, 1};
+  auto referenceManagerPtr = getExp0ReferenceManager(initEventTimes, modeSequence);
+  auto problem = createExp0Problem(referenceManagerPtr);
+  auto ipmProblem = 
 
-  double startTime = 0.0;
-  double finalTime = 2.0;
+  const scalar_t startTime = 0.0;
+  const scalar_t finalTime = 2.0;
+  const vector_t initState = (vector_t(STATE_DIM) << 0.0, 2.0).finished();
 
-  // partitioning times
-  std::vector<double> partitioningTimes;
-  partitioningTimes.push_back(startTime);
-  for (const auto e : initEventTimes) {
-    partitioningTimes.push_back(e);
-  }
-  partitioningTimes.push_back(finalTime);
+  auto initializerPtr = std::unique_ptr<Initializer>(new DefaultInitializer(INPUT_DIM));
 
-  Eigen::Vector2d initState(0.0, 2.0);
+  STOC stoc(settings, problem, initializerPtr);
 
   /******************************************************************************************************/
   /******************************************************************************************************/
   /******************************************************************************************************/
-  // system dynamics
-  EXP0_System systemDynamics(modeScheduleManagerPtr);
-  TimeTriggeredRollout<STATE_DIM, INPUT_DIM> timeTriggeredRollout(systemDynamics, rolloutSettings);
+  // STOC
+  // stoc::STOC solver(&timeTriggeredRollout, &systemDerivative, &systemConstraint, &systemCostFunction, &operatingTrajectories,
+  //                                 slqSettings, modeScheduleManagerPtr, nullptr, gddpSettings, nlpSettings);
 
-  // system derivatives
-  EXP0_SystemDerivative systemDerivative(modeScheduleManagerPtr);
+  // // run ocs2 using LQ method for computing the derivatives
+  // ocs2.gddpSettings().useLQForDerivatives_ = true;
+  // ocs2.run(startTime, initState, finalTime, partitioningTimes, initEventTimes);
+  // Eigen::VectorXd optimizedEventTimes_LQ;
+  // ocs2.getParameters(optimizedEventTimes_LQ);
+  // double optimizedCost_LQ;
+  // ocs2.getCost(optimizedCost_LQ);
 
-  // system constraints
-  ConstraintBase systemConstraint;
+  // // run ocs2 using BVP method for computing the derivatives
+  // ocs2.gddpSettings().useLQForDerivatives_ = false;
+  // ocs2.run(startTime, initState, finalTime, partitioningTimes, initEventTimes);
+  // Eigen::VectorXd optimizedEventTimes_BVP;
+  // ocs2.getParameters(optimizedEventTimes_BVP);
+  // double optimizedCost_BVP;
+  // ocs2.getCost(optimizedCost_BVP);
 
-  // system cost functions
-  EXP0_CostFunction systemCostFunction(modeScheduleManagerPtr);
+  // /******************************************************************************************************/
+  // /******************************************************************************************************/
+  // /******************************************************************************************************/
+  // std::cerr << "### Initial event times are:        ["
+  //           << Eigen::Map<Eigen::VectorXd>(initEventTimes.data(), initEventTimes.size()).transpose() << "]\n";
+  // std::cerr << "### Optimum cost LQ method:         " << optimizedCost_LQ << "\n";
+  // std::cerr << "### Optimum event times LQ method:  [" << optimizedEventTimes_LQ.transpose() << "]\n";
+  // std::cerr << "### Optimum cost BVP method:        " << optimizedCost_BVP << "\n";
+  // std::cerr << "### Optimum event times BVP method: [" << optimizedEventTimes_BVP.transpose() << "]\n";
 
-  // system operatingTrajectories
-  Eigen::Matrix<double, STATE_DIM, 1> stateOperatingPoint = Eigen::Matrix<double, STATE_DIM, 1>::Zero();
-  Eigen::Matrix<double, INPUT_DIM, 1> inputOperatingPoint = Eigen::Matrix<double, INPUT_DIM, 1>::Zero();
-  EXP0_SystemOperatingTrajectories operatingTrajectories(stateOperatingPoint, inputOperatingPoint);
+  // const double optimumCost = 9.766;
+  // const std::vector<double> optimumEventTimes{0.1897};
 
-  // 
-  ocs2::OptimalControlProblem problem;
-
-  /******************************************************************************************************/
-  /******************************************************************************************************/
-  /******************************************************************************************************/
-  // OCS2
-  OCS2<STATE_DIM, INPUT_DIM> ocs2(&timeTriggeredRollout, &systemDerivative, &systemConstraint, &systemCostFunction, &operatingTrajectories,
-                                  slqSettings, modeScheduleManagerPtr, nullptr, gddpSettings, nlpSettings);
-
-  // run ocs2 using LQ method for computing the derivatives
-  ocs2.gddpSettings().useLQForDerivatives_ = true;
-  ocs2.run(startTime, initState, finalTime, partitioningTimes, initEventTimes);
-  Eigen::VectorXd optimizedEventTimes_LQ;
-  ocs2.getParameters(optimizedEventTimes_LQ);
-  double optimizedCost_LQ;
-  ocs2.getCost(optimizedCost_LQ);
-
-  // run ocs2 using BVP method for computing the derivatives
-  ocs2.gddpSettings().useLQForDerivatives_ = false;
-  ocs2.run(startTime, initState, finalTime, partitioningTimes, initEventTimes);
-  Eigen::VectorXd optimizedEventTimes_BVP;
-  ocs2.getParameters(optimizedEventTimes_BVP);
-  double optimizedCost_BVP;
-  ocs2.getCost(optimizedCost_BVP);
-
-  /******************************************************************************************************/
-  /******************************************************************************************************/
-  /******************************************************************************************************/
-  std::cerr << "### Initial event times are:        ["
-            << Eigen::Map<Eigen::VectorXd>(initEventTimes.data(), initEventTimes.size()).transpose() << "]\n";
-  std::cerr << "### Optimum cost LQ method:         " << optimizedCost_LQ << "\n";
-  std::cerr << "### Optimum event times LQ method:  [" << optimizedEventTimes_LQ.transpose() << "]\n";
-  std::cerr << "### Optimum cost BVP method:        " << optimizedCost_BVP << "\n";
-  std::cerr << "### Optimum event times BVP method: [" << optimizedEventTimes_BVP.transpose() << "]\n";
-
-  const double optimumCost = 9.766;
-  const std::vector<double> optimumEventTimes{0.1897};
-
-  ASSERT_NEAR(optimizedCost_LQ, optimumCost, 10 * slqSettings.ddpSettings_.minRelCost_)
-      << "MESSAGE: OCS2 failed in the EXP1 using LQ approach for calculating derivatives!";
-  ASSERT_NEAR(optimizedCost_BVP, optimumCost, 10 * slqSettings.ddpSettings_.minRelCost_)
-      << "MESSAGE: OCS2 failed in the EXP1 using BVP approach for calculating derivatives!";
+  // ASSERT_NEAR(optimizedCost_LQ, optimumCost, 10 * slqSettings.ddpSettings_.minRelCost_)
+  //     << "MESSAGE: OCS2 failed in the EXP1 using LQ approach for calculating derivatives!";
+  // ASSERT_NEAR(optimizedCost_BVP, optimumCost, 10 * slqSettings.ddpSettings_.minRelCost_)
+  //     << "MESSAGE: OCS2 failed in the EXP1 using BVP approach for calculating derivatives!";
 }
 
 int main(int argc, char** argv) {
